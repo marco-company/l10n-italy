03cf79160fe979295da76160528bd397523fceb4
VPA <valerio.paretta@innovyou.it>
2025-03-05 14:50:13 +0100

    [ADD]l10n_it_reverse_charge_start_end_dates: Added module
    This module removes start_date and end_date in account move lines for generated self invoices.

---
+++ l10n_it_reverse_charge_start_end_dates/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Sale Force Invoiced Amount",
+    "version": "16.0.1.0.0",
+    "author": "Innovyou, Odoo Community Association (OCA)",
+    "category": "Hidden",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-italy",
+    "depends": ["account_invoice_start_end_dates", "l10n_it_reverse_charge"],
+    "installable": True,
+    "auto_install": True,
+}
